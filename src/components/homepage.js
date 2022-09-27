5830bab02d52ef5e3fc5a970e0b8a06a8d7aa589
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-09-27 18:25:04 -0500

    modal dalete

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,9 +4,41 @@ import {
 
 let editStatus = false;
 let id = '';
+
+function closeModal() {
+  const divPosts = document.getElementById('div-View');
+  const modalPadre = document.getElementById('modal-content');
+  divPosts.removeChild(modalPadre);
+}
+function modalDelete(item) {
+  const divPosts = document.getElementById('div-View');
+  const modalPadre = document.createElement('section');
+  modalPadre.className = 'modal-class';
+  modalPadre.id = 'modal-content';
+  const modalHijo = document.createElement('section');
+  modalHijo.className = 'modalHijo-class';
+  modalHijo.id = 'modalHijo-content';
+  const textModal = document.createElement('h3');
+  const cancelbtn = document.createElement('button');
+  cancelbtn.className = 'btnCancel';
+  const okbtn = document.createElement('button');
+  okbtn.className = 'btnok';
+  okbtn.textContent = 'Eliminar';
+  okbtn.data = ('data-id', item);
+  cancelbtn.textContent = 'Cancelar';
+
+  textModal.textContent = 'Deseas eliminar este Post?';
+  okbtn.addEventListener('click', ({ target: { data } }) => {
+    deletePost(data);
+  });
+
+  modalHijo.append(textModal, cancelbtn, okbtn);
+  divPosts.appendChild(modalPadre);
+  modalPadre.appendChild(modalHijo);
+}
+
 const formHomePage = () => {
   const postEditSave = document.getElementById('post-description').value;
-  // inputDescription.value = '';
   if (!editStatus) {
     savePost(postEditSave);
   } else {
@@ -44,44 +76,40 @@ export const homepage = () => {
   onGetPost((querySnapshot) => {
     divPosts.innerHTML = '';
     querySnapshot.forEach((doc) => {
-      // console.log(doc.id);
       const collectionPost = doc.data();
-      const allPost = document.createElement('section');
-      allPost.className = 'card-post';
+
       const postContent = document.createElement('p');
       postContent.textContent = collectionPost.post;
       const editBtn = document.createElement('button');
       editBtn.className = 'btn-edit';
       editBtn.data = ('data-id', doc.id);
+      console.log(editBtn.data);
       editBtn.textContent = 'Editar';
       const deleteBtn = document.createElement('button');
       deleteBtn.className = 'btn-delete';
       deleteBtn.data = ('data-id', doc.id);
-      deleteBtn.textContent = 'Eliminar';
 
-      divPosts.append(allPost, postContent, editBtn, deleteBtn);
+      divPosts.append(postContent, editBtn, deleteBtn);
     });
 
     const editPost = divPosts.querySelectorAll('.btn-edit');
+    console.log(editPost);
     editPost.forEach((btn) => {
       btn.addEventListener('click', async (e) => {
         const postId = await getPost(e.target.data);
         const postInfo = postId.data();
-        console.log(postInfo);
-
         inputDescription.value = postInfo.post;
         editStatus = true;
         id = postId.id;
         btnPost.innerText = 'Guardar';
       });
     });
+
     const btnsDelete = divPosts.querySelectorAll('.btn-delete');
     console.log(btnsDelete);
     btnsDelete.forEach((btn) => {
-      btn.addEventListener('click', ({ target: { data } }) => {
-        if (window.confirm('¿Deseas eliminar este Post?')) {
-          deletePost(data);
-        }
+      btn.addEventListener('click', (e) => {
+        (modalDelete(e.target.data));
       });
     });
   });
